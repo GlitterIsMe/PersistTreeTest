9bd90daf74c3c87cd75cb5fa3ecfb230bca6a0f2
zhangyiwen <zzyyyww9595@gmail.com>
2019-06-15 16:03:25 +0800

    refine format

--- persistent_skip_list.cc
+++ persistent_skip_list.cc
@@ -2,11 +2,14 @@
 #include "persistent_skip_list.h"
 #include "statistic.h"
 #include "error.h"
+
 using std::cout;
 namespace rocksdb {
 
     // class Persistent_SkipList
-    Persistent_SkipList::Persistent_SkipList(PersistentAllocator* allocator, int32_t max_height, int32_t branching_factor, size_t key_size ,uint64_t opt_num,size_t per_1g_num)
+    Persistent_SkipList::Persistent_SkipList(PersistentAllocator *allocator, int32_t max_height,
+                                             int32_t branching_factor, size_t key_size, uint64_t opt_num,
+                                             size_t per_1g_num)
             :
             allocator_(allocator),
             kMaxHeight_(static_cast<uint16_t>(max_height)),
@@ -25,36 +28,36 @@ namespace rocksdb {
         per_1g_num_ = per_1g_num;
 #endif
         try {
-            prev_ = reinterpret_cast<Node**>(allocator_->Allocate(sizeof(Node*) * kMaxHeight_));
-        }catch (AllocatorSpaceRunOut& e){
-            cout<<"NVM space is running out"<<"\n";
+            prev_ = reinterpret_cast<Node **>(allocator_->Allocate(sizeof(Node *) * kMaxHeight_));
+        } catch (AllocatorSpaceRunOut &e) {
+            cout << "NVM space is running out" << "\n";
             exit(-1);
         }
         for (int i = 0; i < kMaxHeight_; i++) {
             head_->SetNext(i, nullptr);
             prev_[i] = head_;
-            pmem_flush(prev_ + i, sizeof(Node*));
+            pmem_flush(prev_ + i, sizeof(Node *));
         }
         pmem_drain();
         prev_height_ = 1;
     }
 
-    Node* Persistent_SkipList::NewNode(const std::string &key, int height) {
+    Node *Persistent_SkipList::NewNode(const std::string &key, int height) {
         bool is_pmem = allocator_->is_pmem();
         try {
-            char* mem = allocator_->Allocate(sizeof(Node) + sizeof(Node*) * (height - 1));
-            char* pkey = allocator_->Allocate(key.size());
-        }catch (AllocatorSpaceRunOut& e){
-            cout<<"NVM space is running out"<<"\n";
+            char *mem = allocator_->Allocate(sizeof(Node) + sizeof(Node *) * (height - 1));
+            char *pkey = allocator_->Allocate(key.size());
+        } catch (AllocatorSpaceRunOut &e) {
+            cout << "NVM space is running out" << "\n";
             exit(-1);
         }
-        if(is_pmem){
+        if (is_pmem) {
             pmem_memcpy_persist(pkey, key.c_str(), key.size());
-        }else{
+        } else {
             memcpy(pkey, key.c_str(), key.size());
             pmem_msync(pkey, key.size());
         }
-        return new (mem) Node(pkey);
+        return new(mem) Node(pkey);
     }
 
     int Persistent_SkipList::RandomHeight() {
@@ -66,18 +69,18 @@ namespace rocksdb {
         return height;
     }
 
-    bool Persistent_SkipList::KeyIsAfterNode(const std::string& key, Node* n, Statistic &stat) const {
+    bool Persistent_SkipList::KeyIsAfterNode(const std::string &key, Node *n, Statistic &stat) const {
         return (n != nullptr) && (strncmp(n->key_, key.c_str(), key_size_) < 0);
     }
 
-    int Persistent_SkipList::CompareKeyAndNode(const std::string& key, Node* n) {
+    int Persistent_SkipList::CompareKeyAndNode(const std::string &key, Node *n) {
         int res = strncmp(key.c_str(), n->key_, key_size_);        // 16 equal KEY_SIZE
         return res;
     }
 
     // ignore function name
-    void Persistent_SkipList::FindLessThan(const std::string &key, Node** prev) {
-        Node* x = head_;
+    void Persistent_SkipList::FindLessThan(const std::string &key, Node **prev) {
+        Node *x = head_;
         int level = GetMaxHeight() - 1;
 #ifdef CAL_ACCESS_COUNT
         uint64_t cnt = 0;
@@ -102,14 +105,13 @@ namespace rocksdb {
         cnt_time_ += cnt;
         if(opt_1g_num_%per_1g_num_==0){
             printf("every 1GB(%dGB): times: %.1f\n", (opt_1g_num_ / per_1g_num_), 1.0*cnt_time_/per_1g_num_);
-		//	printf("every 64MB(%d/64MB): times: %.1f\n", (opt_1g_num_ / per_1g_num_), 1.0*cnt_time_/per_1g_num_);
-			cnt_time_ = 0;
-        } 
+        //	printf("every 64MB(%d/64MB): times: %.1f\n", (opt_1g_num_ / per_1g_num_), 1.0*cnt_time_/per_1g_num_);
+            cnt_time_ = 0;
+        }
 #endif
     }
 
-    void Persistent_SkipList::FindNextNode(const std::string &key, Node** prev, Statistic &stats)
-    {
+    void Persistent_SkipList::FindNextNode(const std::string &key, Node **prev, Statistic &stats) {
         // 从prev[level]节点往后查找合适的node
         int level = GetMaxHeight() - 1;
         //Node* x = prev[level];
@@ -118,7 +120,7 @@ namespace rocksdb {
         for (int i = level; i >= 0; i--) {
             // next初始化为x的next，第一层为head
             // 后面就是查找的地方的next
-            Node* next = x->Next(i);
+            Node *next = x->Next(i);
             stats.add_node_search();
             while (next != nullptr && KeyIsAfterNode(key, next, stats)) {
                 x = next;
@@ -129,7 +131,7 @@ namespace rocksdb {
         }
     }
 
-    void Persistent_SkipList::Insert(const std::string &key, Statistic& stats) {
+    void Persistent_SkipList::Insert(const std::string &key, Statistic &stats) {
         // 比较待插入节点与prev[0]的关系
         stats.start();
         /*if(!KeyIsAfterNode(key, prev_[0]->Next(0)) &&
@@ -146,31 +148,31 @@ namespace rocksdb {
         stats.add_search();
 
         int height = RandomHeight();
-        if(height > GetMaxHeight()){
-            for(int i = GetMaxHeight(); i < height; i++){
+        if (height > GetMaxHeight()) {
+            for (int i = GetMaxHeight(); i < height; i++) {
                 prev_[i] = head_;
             }
             max_height_ = static_cast<uint16_t>(height);
         }
 
         stats.start();
-        Node* x = NewNode(key, height);
+        Node *x = NewNode(key, height);
         bool is_pmem = allocator_->is_pmem();
-        for(int i = 0; i < height; i++){
+        for (int i = 0; i < height; i++) {
             x->SetNext(i, prev_[i]->Next(i));
-            prev_[i]->SetNext(i ,x);
-            if(is_pmem){
-                pmem_persist(prev_[i], sizeof(Node*));
-            }else{
-                pmem_msync(prev_[i], sizeof(Node*));
+            prev_[i]->SetNext(i, x);
+            if (is_pmem) {
+                pmem_persist(prev_[i], sizeof(Node *));
+            } else {
+                pmem_msync(prev_[i], sizeof(Node *));
             }
             //prev_[i] = x;
         }
         prev_[0] = x;
-        if(is_pmem){
-            pmem_persist(prev_[0], sizeof(Node*));
-        }else{
-            pmem_msync(prev_[0], sizeof(Node*));
+        if (is_pmem) {
+            pmem_persist(prev_[0], sizeof(Node *));
+        } else {
+            pmem_msync(prev_[0], sizeof(Node *));
         }
         stats.end();
         stats.add_write();
@@ -179,29 +181,29 @@ namespace rocksdb {
     }
 
     std::string Persistent_SkipList::Get(const std::string &key) {
-        Node* grt_or_equal = FindGreaterOrEqual(key);
-        if(grt_or_equal != nullptr){
+        Node *grt_or_equal = FindGreaterOrEqual(key);
+        if (grt_or_equal != nullptr) {
             int cmp = strncmp(grt_or_equal->key_, key.c_str(), key_size_);
-            if(cmp == 0){
+            if (cmp == 0) {
                 return std::string(grt_or_equal->key_, strlen(grt_or_equal->key_));
-            }else{
+            } else {
                 return string("");
             }
-        }else{
+        } else {
             return string("");
         }
     }
 
-    Node* Persistent_SkipList::FindGreaterOrEqual(const std::string &key) const {
-        Node* x = head_;
+    Node *Persistent_SkipList::FindGreaterOrEqual(const std::string &key) const {
+        Node *x = head_;
         int level = GetMaxHeight() - 1;
-        Node* last_bigger;
-        while(true){
-            Node* next = x->Next(level);
+        Node *last_bigger;
+        while (true) {
+            Node *next = x->Next(level);
             int cmp = (next == nullptr || next == last_bigger) ? 1 : strncmp(next->key_, key.c_str(), key_size_);
-            if(cmp == 0 || (cmp > 0 && level ==0)){
+            if (cmp == 0 || (cmp > 0 && level == 0)) {
                 return next;
-            } else if(cmp < 0) {
+            } else if (cmp < 0) {
                 x = next;
             } else {
                 last_bigger = next;
@@ -210,7 +212,7 @@ namespace rocksdb {
         }
     }
 
-    void Persistent_SkipList::PrintKey(const char *str ,uint64_t &last_num, uint64_t &last_seq_num) const {
+    void Persistent_SkipList::PrintKey(const char *str, uint64_t &last_num, uint64_t &last_seq_num) const {
         // uint64_t key_num, seq_num;
         // key_num = 0;    seq_num = 0;
 
@@ -226,7 +228,7 @@ namespace rocksdb {
         for (int i = 0; i < 8; i++) {
             num = (str[i] - '0') + 10 * num;
         }
-        
+
         for (int i = 8; i < 18; i++) {
             seq_num = (str[i] - '0') + 10 * seq_num;
         }
@@ -242,24 +244,24 @@ namespace rocksdb {
     }
 
     void Persistent_SkipList::Print() const {
-        Node* start = head_->Next(0);
+        Node *start = head_->Next(0);
         uint64_t last_num = 0;
         uint64_t last_seq_num = 0;
-        while(start != nullptr) {
-            PrintKey(start->key_,last_num, last_seq_num);
+        while (start != nullptr) {
+            PrintKey(start->key_, last_num, last_seq_num);
             start = start->Next(0);
         }
     }
 
     void Persistent_SkipList::PrintLevelNum() const {
-        for(int i=0;i<kMaxHeight_;i++){
-            Node* start = head_->Next(i);
-            int num=0;
-            while(start != nullptr){
+        for (int i = 0; i < kMaxHeight_; i++) {
+            Node *start = head_->Next(i);
+            int num = 0;
+            while (start != nullptr) {
                 num++;
-                start=start->Next(i);
+                start = start->Next(i);
             }
-            printf("Level %d = %d\n",i,num);
+            printf("Level %d = %d\n", i, num);
         }
         printf("\n");
     }
